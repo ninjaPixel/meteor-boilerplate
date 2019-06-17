dae928368e696410669af806ae07253597a800bd
ninjaPixel <matthew@ninjaPixel.eu>
2019-06-17 17:46:40 +0100

    Add a schema to the Sample collection

--- app/imports/api/SampleCollection/SampleCollection.js
+++ app/imports/api/SampleCollection/SampleCollection.js
@@ -1,5 +1,15 @@
+import SimpleSchema from 'simpl-schema';
 import newCollection from '../../modules/newCollection';
 
 const SampleCollection = newCollection('sampleCollection');
+
+const schema = new SimpleSchema({
+  count: {
+    type: Number,
+    label: 'A number',
+  },
+});
+
+SampleCollection.attachSchema(schema);
 
 export default SampleCollection;
