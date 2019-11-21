6a10be9c96b4c0f1c6ebf81d08a46c1238ee242e
ninjaPixel <matthew@ninjaPixel.eu>
2019-11-21 09:23:22 +0000

    wip story

--- app/stories/3-Layouts.stories.js
+++ app/stories/3-Layouts.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import Typography from '@material-ui/core/Typography';
 import ResponsivePaper from '../view/components/ResponsivePaper/ResponsivePaper';
 import StoryWrapper from './StoryWrapper';
 
@@ -11,8 +12,19 @@ export const responsivePaper = () => {
   return (
     <div>
       <StoryWrapper>
-        <div style={{ width: '300px' }}>
-          <ResponsivePaper title="Container width = 300px">...contents</ResponsivePaper>
+        <p>Container full width</p>
+        <div style={{ border: '1px dotted white', padding: '8px' }}>
+          <ResponsivePaper title="Component title">
+            <Typography>
+              When the container is wide enough, this component will split into a title and then a card. If the
+              container is too narrow for this (think on a mobile) then the title and contents will be placed one after
+              the other, without a card, for a simpler look.
+            </Typography>
+          </ResponsivePaper>
+        </div>
+        <p>Container width = 300px</p>
+        <div style={{ width: '300px', border: '1px dotted white', padding: '8px' }}>
+          <ResponsivePaper title="Component title">...contents</ResponsivePaper>
         </div>
       </StoryWrapper>
     </div>
